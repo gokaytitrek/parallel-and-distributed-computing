2d27905a3eac3ea6fb67e2fe115d8965df95badb
Gokay Titrek <gokaytitrek@hotmail.com>
2014-12-20 05:17:55 +0200

    - Node ların mesaj gondermesi ve gelen mesajları dinlemesi eklendi.

--- CausallyOrderedMulticastComm/src/causallyOrderedMulticastComm/Node.java
+++ CausallyOrderedMulticastComm/src/causallyOrderedMulticastComm/Node.java
@@ -5,60 +5,172 @@
  */
 package causallyOrderedMulticastComm;
 
-import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JTextArea;
 
 
 /**
  *
  * @author Gokay
  */
-public class Node {
-    	private static final String serverName = "localhost";
-	InetAddress group;
-        
-	protected String _name;
-	protected int _portNumber;
-	protected int _portNumberOtherNode;
-//	protected boolean _setupNode;
-//	protected boolean _firstNode;
-//	protected ArrayList<Node> _nodeList=new ArrayList<Node>();
-//	protected ServerSocket _serverSocket;
-//        protected Socket clientSocket;
-//	protected PrintWriter socketOut;
-//	protected BufferedReader socketIn;
+public class Node implements Runnable{
+    	private static final String _serverName = "localhost";
+	private InetAddress _group;
+        private MulticastSocket _multicastSocket;
+	public String _nodeName;
+	private int _portNumber = 6000;
+        protected ArrayList<ArrayList<Integer>> _queue;
+        private InetAddress _multicastAddress;
+        private InetAddress _localHost;
+        private int _sleepTime;
+        public JTextArea _textArea;
 
-    public Node() {
-                try {
-                    this.group = InetAddress.getByName("228.5.6.7");
-                } catch (UnknownHostException ex) {
-                    Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
-                }
+    public Node(String Name,int SleepTime) throws UnknownHostException {
+        
+       
+            this._nodeName = Name;
+            this._sleepTime = SleepTime;
+            this._group = InetAddress.getByName("228.5.6.7");
+            this._multicastAddress = InetAddress.getByName("230.0.0.1");
+            this._localHost = InetAddress.getLocalHost();
+          
+            _textArea = new JTextArea(1000, 0);
+            _textArea.setEditable(false);
     }
     
-    public void sendMessage() throws IOException
+    public void sendMessage(String Message) throws IOException, InterruptedException
     {
-        String msg = "Hello";
-        MulticastSocket s = new MulticastSocket(6789);
-        s.joinGroup(this.group);
-        DatagramPacket hi = new DatagramPacket(msg.getBytes(), msg.length(),group, 6789);
-        s.send(hi);
+        DatagramSocket dgramSocket = new DatagramSocket();
+        DatagramPacket packet = null;
+      try {
+
+        // serialize the multicast message
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream out = new ObjectOutputStream (bos);
+        out.writeObject(new String("Message : " + Message));
+        out.flush();
+        out.close();
+
+        // Create a datagram packet and send it
+
+        packet = new DatagramPacket(bos.toByteArray(),
+                                    bos.size(),
+                                    _multicastAddress,
+                                    _portNumber);
+
+
+
+        // send the packet
+
+        dgramSocket.send(packet);
+
+        System.out.println("sending multicast message");
+      }catch (IOException ioe) {
+
+        System.out.println("error sending multicast");
+
+        ioe.printStackTrace(); System.exit(1);
+
+      }
         
-         // get their responses!
+    }
+    
+    public void run() {
+
+    try {
+
+      System.out.println("Setting up multicast receiver");
+
+      _multicastSocket = new MulticastSocket(_portNumber);
+
+      _multicastSocket.joinGroup(_multicastAddress);
+
+    } catch(IOException ioe) {
+
+      System.out.println("Trouble opening multicast port");
+
+      ioe.printStackTrace();      System.exit(1);
+
+    }
+
+
+    DatagramPacket packet;
+
+    System.out.println("Multicast receiver set up ");
+
+    while (true) {
+
+      try {
+        Thread.sleep(this._sleepTime);
         byte[] buf = new byte[1000];
-        DatagramPacket recv = new DatagramPacket(buf, buf.length);
-        s.receive(recv);
-        // OK, I'm done talking - leave the group...
-        s.leaveGroup(group);
+
+        packet = new DatagramPacket(buf,buf.length);
+
+        //System.out.println("McastReceiver: waiting for packet");
+
+        _multicastSocket.receive(packet);
+
+        
+
+        ByteArrayInputStream bistream = 
+
+          new ByteArrayInputStream(packet.getData());
+
+        ObjectInputStream ois = new ObjectInputStream(bistream);
+
+        String value = (String) ois.readObject();
+
+        _textArea.append(value + "\n");
+
+        // ignore packets from myself, print the rest
+
+        //if (!(packet.getAddress().equals(localHost))) {
+
+          System.out.println(this._nodeName +" Received multicast packet: "+
+
+                           value + " from: " + packet.getAddress());
+
+        //} 
+
+        ois.close();
+
+        bistream.close();
+
+      } catch(IOException ioe) {
+
+        System.out.println("Trouble reading multicast message");
+
+        ioe.printStackTrace();  System.exit(1);
+
+      } catch (ClassNotFoundException cnfe) {
+
+        System.out.println("Class missing while reading mcast packet");
+
+        cnfe.printStackTrace(); System.exit(1);
+
+      } catch (InterruptedException ex) {
+            Logger.getLogger(Node.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+  }
+    
+    public void closeConnection() throws IOException
+    {
+        this._multicastSocket.leaveGroup(_multicastAddress);
     }
 }
